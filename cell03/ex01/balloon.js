488cc5ecc7b55cebc8c8a7d62b50415b2f2acd66
Tanabordi <tanabm115@gmail.com>
2026-09-16 16:37:20 +0700

    ex01

---
+++ cell03/ex01/balloon.js
@@ -0,0 +1,30 @@
+const balloon = document.getElementById('balloon');
+let size = 200;
+const colors = ['red', 'green', 'blue'];
+let colorIndex = 0;
+
+balloon.addEventListener('click', function() {
+    size += 10;
+    if (size > 420) {
+        size = 200;
+    }
+    
+    colorIndex = (colorIndex + 1) % 3;
+    updateBalloon();
+});
+
+balloon.addEventListener('mouseleave', function() {
+    size -= 5;
+    if (size < 200) {
+        size = 200;
+    }
+    
+    colorIndex = (colorIndex - 1 + 3) % 3;
+    updateBalloon();
+});
+
+function updateBalloon() {
+    balloon.style.width = size + 'px';
+    balloon.style.height = size + 'px';
+    balloon.style.backgroundColor = colors[colorIndex];
+}
